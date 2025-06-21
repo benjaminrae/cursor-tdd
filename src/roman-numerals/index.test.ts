b0cc20913bd5b536328d0139de2eda2b0694ed5f
Benjamin Rae <benjaminrae93@gmail.com>
2025-06-21 12:31:32 +0200

    refactor(roman-numerals): extract output variable in each test

--- src/roman-numerals/index.test.ts
+++ src/roman-numerals/index.test.ts
@@ -11,33 +11,36 @@ describe('RomanNumeralConverter', () => {
   it('should return empty string for zero', () => {
     // Arrange
     const input = 0;
+    const expected = "";
     
     // Act
     const result = converter.toRoman(input);
     
     // Assert
-    expect(result).toBe("");
+    expect(result).toBe(expected);
   });
 
   it('should return "I" for one', () => {
     // Arrange
     const input = 1;
+    const expected = "I";
     
     // Act
     const result = converter.toRoman(input);
     
     // Assert
-    expect(result).toBe("I");
+    expect(result).toBe(expected);
   });
 
   it('should return "II" for two', () => {
     // Arrange
     const input = 2;
+    const expected = "II";
     
     // Act
     const result = converter.toRoman(input);
     
     // Assert
-    expect(result).toBe("II");
+    expect(result).toBe(expected);
   });
 }); 
