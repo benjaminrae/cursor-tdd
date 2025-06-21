18867c28ab367bc0ea2e3068df3d95b7c6382ba6
Benjamin Rae <benjaminrae93@gmail.com>
2025-06-21 12:32:45 +0200

    refactor(roman-numerals): parameterize test

--- src/roman-numerals/index.test.ts
+++ src/roman-numerals/index.test.ts
@@ -8,35 +8,11 @@ describe('RomanNumeralConverter', () => {
     converter = new RomanNumeralConverter();
   });
 
-  it('should return empty string for zero', () => {
-    // Arrange
-    const input = 0;
-    const expected = "";
-    
-    // Act
-    const result = converter.toRoman(input);
-    
-    // Assert
-    expect(result).toBe(expected);
-  });
-
-  it('should return "I" for one', () => {
-    // Arrange
-    const input = 1;
-    const expected = "I";
-    
-    // Act
-    const result = converter.toRoman(input);
-    
-    // Assert
-    expect(result).toBe(expected);
-  });
-
-  it('should return "II" for two', () => {
-    // Arrange
-    const input = 2;
-    const expected = "II";
-    
+  it.each([
+    [0, ""],
+    [1, "I"],
+    [2, "II"]
+  ])('should return "%s" for %i', (input, expected) => {
     // Act
     const result = converter.toRoman(input);
     
