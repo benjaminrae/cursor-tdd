ab43e2e294e92f50b021f4ec8210eec1958de4ab
Benjamin Rae <benjaminrae93@gmail.com>
2025-06-21 12:58:18 +0200

    test(roman-numerals): include other test cases that are already passing

--- src/roman-numerals/index.test.ts
+++ src/roman-numerals/index.test.ts
@@ -12,9 +12,11 @@ describe('RomanNumeralConverter', () => {
     [0, ""],
     [1, "I"],
     [2, "II"],
+    [3, "III"],
     [5, "V"],
     [6, "VI"],
-    [7, "VII"]
+    [7, "VII"],
+    [8, "VIII"]
   ])('should return "%s" for %i', (input, expected) => {
     // Act
     const result = converter.toRoman(input);
