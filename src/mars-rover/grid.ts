8b59cf7dc38b49a8cb728a0778b912791f102abb
Benjamin Rae <benjaminrae93@gmail.com>
2025-06-23 23:05:50 +0200

    refactor(mars-rover): add GRID_SIZE constant to eliminate magic number

--- src/mars-rover/grid.ts
+++ src/mars-rover/grid.ts
@@ -1,5 +1,7 @@
+const GRID_SIZE = 10;
+
 export class Grid {
-  constructor(private size: number = 10) {}
+  constructor(private size: number = GRID_SIZE) {}
 
   wrapY(yMovement: number): number {
     return yMovement % this.size;
